6921e570209d92e24754720bd201fbd42f9b0aa2
Curious <Curious@curious.host>
2023-03-15 14:36:15 +0800

    Update cm520-openwrt-stable-custom-script-1.sh

--- cm520-openwrt-stable-custom-script-1.sh
+++ cm520-openwrt-stable-custom-script-1.sh
@@ -36,99 +36,111 @@
 
 touch opboot.patch
 cat>opboot.patch<<EOF
-Subject: [PATCH] =?UTF-8?q?=E9=80=82=E9=85=8Dopboot?=
-MIME-Version: 1.0
-Content-Type: text/plain; charset=UTF-8
-Content-Transfer-Encoding: 8bit
+From 0003ff9e977f675b03c2a472c98b3fba634b15c1 Mon Sep 17 00:00:00 2001
+From: Curious <Curious@curious.host>
+Date: Wed, 15 Mar 2023 14:32:33 +0800
+Subject: [PATCH] ipq40xx: adapt the DTS of MobiPromo CM520-79F to opboot
 
-修改nand分区以适配opboot
 ---
- .../arm/boot/dts/qcom-ipq4019-cm520-79f.dts   | 64 +------------------
- 1 file changed, 3 insertions(+), 61 deletions(-)
+ .../arm/boot/dts/qcom-ipq4019-cm520-79f.dts   | 68 ++-----------------
+ 1 file changed, 7 insertions(+), 61 deletions(-)
 
 diff --git a/target/linux/ipq40xx/files/arch/arm/boot/dts/qcom-ipq4019-cm520-79f.dts b/target/linux/ipq40xx/files/arch/arm/boot/dts/qcom-ipq4019-cm520-79f.dts
-index 1dde17e293..669fd5a784 100644
+index 1dde17e293..1224a0cb9e 100644
 --- a/target/linux/ipq40xx/files/arch/arm/boot/dts/qcom-ipq4019-cm520-79f.dts
 +++ b/target/linux/ipq40xx/files/arch/arm/boot/dts/qcom-ipq4019-cm520-79f.dts
-@@ -196,66 +196,8 @@
- 			#size-cells = <1>;
- 
- 			partition@0 {
--				label = "SBL1";
--				reg = <0x0 0x100000>;
--				read-only;
--			};
+@@ -16,6 +16,10 @@
+                led-upgrade = &led_sys;
+        };
+
++       chosen {
++               bootargs-append = " ubi.block=0,1 root=/dev/ubiblock0_1";
++       };
++
+        soc {
+                rng@22000 {
+                        status = "okay";
+@@ -196,66 +200,8 @@
+                        #size-cells = <1>;
+
+                        partition@0 {
+-                               label = "SBL1";
+-                               reg = <0x0 0x100000>;
+-                               read-only;
+-                       };
 -
--			partition@100000 {
--				label = "MIBIB";
--				reg = <0x100000 0x100000>;
--				read-only;
--			};
+-                       partition@100000 {
+-                               label = "MIBIB";
+-                               reg = <0x100000 0x100000>;
+-                               read-only;
+-                       };
 -
--			partition@200000 {
--				label = "BOOTCONFIG";
--				reg = <0x200000 0x100000>;
--			};
+-                       partition@200000 {
+-                               label = "BOOTCONFIG";
+-                               reg = <0x200000 0x100000>;
+-                       };
 -
--			partition@300000 {
--				label = "QSEE";
--				reg = <0x300000 0x100000>;
--				read-only;
--			};
+-                       partition@300000 {
+-                               label = "QSEE";
+-                               reg = <0x300000 0x100000>;
+-                               read-only;
+-                       };
 -
--			partition@400000 {
--				label = "QSEE_1";
--				reg = <0x400000 0x100000>;
--				read-only;
--			};
+-                       partition@400000 {
+-                               label = "QSEE_1";
+-                               reg = <0x400000 0x100000>;
+-                               read-only;
+-                       };
 -
--			partition@500000 {
--				label = "CDT";
--				reg = <0x500000 0x80000>;
--				read-only;
--			};
+-                       partition@500000 {
+-                               label = "CDT";
+-                               reg = <0x500000 0x80000>;
+-                               read-only;
+-                       };
 -
--			partition@580000 {
--				label = "CDT_1";
--				reg = <0x580000 0x80000>;
--				read-only;
--			};
+-                       partition@580000 {
+-                               label = "CDT_1";
+-                               reg = <0x580000 0x80000>;
+-                               read-only;
+-                       };
 -
--			partition@600000 {
--				label = "BOOTCONFIG1";
--				reg = <0x600000 0x80000>;
--			};
+-                       partition@600000 {
+-                               label = "BOOTCONFIG1";
+-                               reg = <0x600000 0x80000>;
+-                       };
 -
--			partition@680000 {
--				label = "APPSBLENV";
--				reg = <0x680000 0x80000>;
--			};
+-                       partition@680000 {
+-                               label = "APPSBLENV";
+-                               reg = <0x680000 0x80000>;
+-                       };
 -
--			partition@700000 {
--				label = "APPSBL";
--				reg = <0x700000 0x200000>;
--				read-only;
--			};
+-                       partition@700000 {
+-                               label = "APPSBL";
+-                               reg = <0x700000 0x200000>;
+-                               read-only;
+-                       };
 -
--			partition@900000 {
--				label = "APPSBL_1";
--				reg = <0x900000 0x200000>;
--				read-only;
-+				label = "Bootloader";
-+				reg = <0x0 0xb00000>;
- 			};
- 
- 			art: partition@b00000 {
-@@ -284,7 +226,7 @@
- 			};
- 
- 			partition@b80000 {
--				label = "ubi";
-+				label = "rootfs";
- 				reg = <0xb80000 0x7480000>;
- 			};
- 		};
--- 
+-                       partition@900000 {
+-                               label = "APPSBL_1";
+-                               reg = <0x900000 0x200000>;
+-                               read-only;
++                               label = "Bootloader";
++                               reg = <0x0 0xb00000>;
+                        };
+
+                        art: partition@b00000 {
+@@ -284,7 +230,7 @@
+                        };
+
+                        partition@b80000 {
+-                               label = "ubi";
++                               label = "rootfs";
+                                reg = <0xb80000 0x7480000>;
+                        };
+                };
+--
+2.30.2
+
 EOF
 git apply opboot.patch
 
